a7fde1d04330723bec8b236cd70e9933e0d35a06
d38dm8nw81k1ng <d38dm8nw81k1ng@localhost>
2009-01-15 21:53:49 +0000

    Created the gNet file and corrected some indentation and syntax errors in gFS

---
+++ trunk/gNet.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+#
+#   gNet.py
+#       
+#   Copyright 2009 Scott C. Walton <d38dm8nw81k1ng@gmail.com>
+#       
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License (version 2), as
+#   oublished by the Free Software Foundation
+#       
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#       
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#   MA 02110-1301, USA.
+
+import gdata.docs.service
+import gdata.docs
+
+class GNet:
+    """
+    Performs all the main interfacing with Google Docs server as well
+    as storing the user's session data
+    """
+    
+
+def main():
+	
+	return 0
+
+if __name__ == '__main__':
+    main()

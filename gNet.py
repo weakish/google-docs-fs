bb85e20d5ea22a2376fa0f3e9ce2c57ee913778c
d38dm8nw81k1ng <d38dm8nw81k1ng@gmail.com>
2009-02-05 18:23:45 +0000

    Rewrote the file system to deal with directories better. Runs much slower due to the way Google's List Data API works. I've lodged a feature request and I'll see what comes of it.

--- gNet.py
+++ gNet.py
@@ -26,26 +26,37 @@ class GNet(object):
     Performs all the main interfacing with Google Docs server as well
     as storing the user's session data
     """
+    
     def __init__(self, em, pw):
         """
         Purpose: Login to Google Docs and store the session cookie
-        em: The user's email address
-        pw: The user's password
+        em: A String containing the user's email address
+        pw: A String containing the user's password
         Returns: A GNet object for accessing the GData Docs
         """
+        
         self.gd_client = gdata.docs.service.DocsService()
         self.gd_client.email = em
         self.gd_client.password = pw
         self.gd_client.source = 'google-docs-fs'
         self.gd_client.ProgrammaticLogin()
-    
-    def get_docs(self, filetypes):
+        
+    def get_docs(self, filetypes = None, folder = None):
         """
         Purpose: Retrieve a list of all documents
-        Returns: A list of all the user's documents
+        filetypes: A List containing the filetypes to query
+        folder: A String containing the folder to search in
+        Returns: A List of documents specified by filetypes: Type gdata.docs.DocumentListFeed
         """
-        query = gdata.docs.service.DocumentQuery(categories=filetypes)
+        
+        query = gdata.docs.service.DocumentQuery(categories = filetypes)
+        query['showfolders'] = 'true'
+        
+        if folder is not None:
+            query['folder'] = folder
+
         return self.gd_client.Query(query.ToUri())
+        
 
 def main():
     """
@@ -53,7 +64,30 @@ def main():
     Returns: 0 to indicate successful execution
     """
     
-    return 0
+    from sys import argv
+    gd_client = gdata.docs.service.DocsService()
+    gd_client.email = argv[1]
+    gd_client.password = argv[2]
+    gd_client.source = 'google-docs-fs'
+    gd_client.ProgrammaticLogin()
+    
+    q = gdata.docs.service.DocumentQuery(categories = None)
+    q['showfolders'] = 'true'
+    q['category'] = 'folder'
+    #q['category'] = '-test'
+    
+    feed = gd_client.Query(q.ToUri())
+
+    print '\n'
+    if(len(feed.entry) == 0):
+        print "No entries in feed.\n"
+    else:
+        #for i in feed.entry:
+        #    print i, "\n"
+        for i, entry in enumerate(feed.entry):
+            print '%s %s %s\n' % (i+1, entry.title.text.encode('UTF-8'), entry.category[0].label)
+        
+    return 0 
 
 if __name__ == '__main__':
     main()

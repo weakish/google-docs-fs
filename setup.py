c6e72ebeac210abe23f2b4a8db69f82532c6ab35
d38dm8nw81k1ng <d38dm8nw81k1ng@localhost>
2010-06-07 19:27:05 +0000

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -7,12 +7,13 @@ required = []
 
 setup(
     name='google-docs-fs',
-    version='1.0rc1',
+    version='1.0rc2',
     description='Treat Google Docs as a local file system',
     author='Scott Walton',
     author_email='d38dm8nw81k1ng@gmail.com',
     license='GPLv2',
     url='http://code.google.com/p/google-docs-fs/',
     py_modules=['googledocsfs.gFile','googledocsfs.gNet'],
-    scripts=['gmount','gumount','gmount.py']
+    scripts=['gmount','gumount','gmount.py'],
+    install_requires=['python-fuse>=0.2','python-gdata>=2.0.0']
     )
